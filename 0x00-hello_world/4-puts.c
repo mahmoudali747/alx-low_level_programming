5573acb120ebf48bdb1e1d9cea522e14ca38b348
mahmoudali747 <mahmoud.ali2962@gmail.com>
2023-06-15 09:28:07 -0700

    haaaaaaaaaaaaaa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  *comment system
  * */
 int main ( void ){
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
